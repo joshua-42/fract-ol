229f0a1cda57c482f5d83f8f5de9ccc491749fbb
Joshua Douglass <jodougla@2A2.42angouleme.fr>
2025-05-14 10:42:01 +0200

    delete the macrolibx library and change the error handle

--- srcs/utils/write_error.c
+++ srcs/utils/write_error.c
@@ -6,7 +6,7 @@
 /*   By: jodougla <jodougla@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 21:36:37 by jodougla          #+#    #+#             */
-/*   Updated: 2025/02/17 15:47:00 by jodougla         ###   ########.fr       */
+/*   Updated: 2025/05/14 10:40:40 by jodougla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <fractol.h>
@@ -15,7 +15,7 @@ void	write_error(void)
 {
 	write(2, "Error enter one of this name\n", 29);
 	write(2, "-Mandelbrot\n", 12);
-	write(2, "-Julia with two float number\n", 27);
+	write(2, "-Julia with two float number\n", 29);
 	write(2, "-Burningship\n", 13);
 	write(2, "-Multibrot with one integer\n", 28);
 }
